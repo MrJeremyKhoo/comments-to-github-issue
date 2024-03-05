5ba47111fee919dfec20f4622d916322e10b5948
Agentum07 <gupta.abhi0710@gmail.com>
2024-03-06 01:44:49 +0800

    improve code quality

--- todo_scanner.py
+++ todo_scanner.py
@@ -6,31 +6,41 @@
 
 import re
 import os
-
+from collections import defaultdict
 def export_to_txt():
     # write file name, line number.
     pass
 
 
-def scan_todos(file_path):
-    print("Inside scan todos")
-    pattern = r"^(#\s*todo):\s*"
-    with open(file_path, "r") as file:
-        for line_num, line in enumerate(file.readlines(), start=1):
-            line = line.strip()
-            line = line.lower()
-            if (re.search(pattern, line)):
-                tokens = list(filter(None, re.split(pattern, line)))
-                print(f"{file_path}, line {line_num}: {tokens[1]}")
+def get_todos(file_paths, pattern):
+    todos = defaultdict(list)
+    for file_path in file_paths:
+        with open(file_path, "r") as file:
+            for line_num, line in enumerate(file.readlines(), start=1):
+                line = line.strip()
+                line = line.lower()
+                if (pattern.search(line)):
+                    tokens = list(filter(None, pattern.split(line)))
+                    # print(f"{file_path}, line {line_num}: {tokens[1]}")
+                    todos[file_path].append(f"line {line_num}: {tokens[1]}")
+    return todos
 
 
-def iterate(dir=os.getcwd(), extension="py"):
-    # path = '/Users/a65888/Documents/UG_Study/Y3S2/CS3203/23s2-cp-spa-team-13/Team13/Code13/src/spa/src/PKB'
+def get_files(dir=os.getcwd(), extension="py"):
     dir = "/Users/a65888/Desktop/todo-summarizer"
+    res = set()
     for root, _, files in os.walk(dir):
       for file in files:
           if file.endswith(".py"):
               file_path = os.path.join(root, file)
-              scan_todos(file_path)
+              res.add(file_path)
+    return res
 
-iterate()
+
+def main():
+    pattern = re.compile(r"^(#\s*todo):\s*")
+    file_paths = get_files()
+    todos = get_todos(file_paths, pattern)
+    print(todos)
+
+main()
