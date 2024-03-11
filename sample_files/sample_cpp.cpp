dad5f730cf38fd0f473137043f003756cebe2818
Agentum07 <gupta.abhi0710@gmail.com>
2024-03-11 12:46:32 +0800

    add sample files

---
+++ sample_files/sample_cpp.cpp
@@ -0,0 +1,22 @@
+// sample.cpp
+#include <iostream>
+
+// Function to add two numbers
+int add(int a, int b) {
+    // todo: Implement the addition logic
+    return a + b;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    // TODO: Add user input functionality
+
+    // Another TODO in a loop
+    for (int i = 0; i < 5; ++i) {
+        // TODO: Improve loop logic
+        std::cout << "Iteration " << i << std::endl;
+    }
+
+    return 0;
+}
