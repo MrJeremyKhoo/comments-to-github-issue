bad802a725ab46404bc38a853ebf52766372f919
MrJeremyKhoo <jeremykhoois@pm.me>
2024-04-09 19:04:42 +0800

    call from a main.py
    call from a main file

---
+++ main.py
@@ -0,0 +1,9 @@
+import todo_scanner
+import sys
+
+def main(args):
+    todo_scanner.main(args);
+
+if __name__ == "__main__":
+    main(sys.argv);
+
--- todo_scanner.py
+++ todo_scanner.py
@@ -74,3 +74,4 @@ def main(args):
 
 if __name__ == "__main__":
     main(sys.argv)
+
